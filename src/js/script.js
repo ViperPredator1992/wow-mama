3fe2326dd7dfe66e3dd3f60ab3ad50ac34693562
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-05-24 01:10:13 +0300

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -1004,4 +1004,21 @@ $(document).ready(function () {
 
     });
 
+    $('.about-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
 });
